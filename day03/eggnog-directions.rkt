4a75df633959bc89cd454eee4b1f5e8c3e3d0050
Ronie Uliana <ronie.uliana@gmail.com>
2015-12-24 12:17:30 -0200

    Day03 - Part 2 Multiple Santas delivering
    We can now deliver presents with several Santas ^_^
    
    Santa Storm!

--- day03/eggnog-directions.rkt
+++ day03/eggnog-directions.rkt
@@ -2,24 +2,37 @@
 
 (provide visited-houses)
 
-(define (visited-houses directions)
-  (dict-count (visit directions)))
+(define (visited-houses directions (number-of-santas 1))
+  (dict-count (visit directions number-of-santas)))
 
-(define (visit directions)
-  (let* ([start-house #hash((#(0 0) . 1))])
-    (walk-directions start-house 0 0 (string->list directions))))
+(define (visit directions number-of-santas)
+  (let* ([start-house (hash #(0 0) number-of-santas)])
+    (move-and-delivery start-house
+                       (make-list number-of-santas #(0 0))
+                       (string->list directions))))
 
-(define (walk-directions houses x y directions)
+(define (move-and-delivery houses santas directions)
   (if (empty? directions) houses
       (let* ([d (first directions)]
-             [new-x (case d
-                      [(#\<) (sub1 x)]
-                      [(#\>) (add1 x)]
-                      [else x])]
-             [new-y (case d
-                      [(#\^) (add1 y)]
-                      [(#\v) (sub1 y)]
-                      [else y])]
-             [position (vector-immutable new-x new-y)]
-             [new-houses (dict-update houses position add1 0)])
-        (walk-directions new-houses new-x new-y (rest directions)))))
+             [santa (first santas)]
+             [new-santa (move santa d)]
+             [new-houses (delivery houses new-santa)])
+        (move-and-delivery new-houses
+                           ; Cycling santas
+                           (append (rest santas) (list new-santa))
+                           (rest directions)))))
+
+; Santa moving
+(define (move position direction)
+  (match position
+    [(vector x y)
+     (case direction
+       [(#\<) (vector (sub1 x) y)]
+       [(#\>) (vector (add1 x) y)]
+       [(#\^) (vector x (add1 y))]
+       [(#\v) (vector x (sub1 y))]
+       [else position])]))
+
+; Houses delivering
+(define (delivery houses position)
+  (dict-update houses position add1 0))
