de704e3bfad5756f109aa1611f7928ad140984ab
Ronie Uliana <ronie.uliana@gmail.com>
2015-12-28 16:29:56 -0200

    Day01 - Calculate Santa floor and until he reaches floor -1

--- day01/floor-test.rkt
+++ day01/floor-test.rkt
@@ -9,3 +9,8 @@
 (check-equal? (where-is-santa "(()(()(") 3)
 (check-equal? (where-is-santa "))(") -1)
 (check-equal? (where-is-santa ")())())") -3)
+
+(check-equal? (moves-to-floor "") 0)
+(check-equal? (moves-to-floor ")") 1)
+(check-equal? (moves-to-floor "()())") 5)
+(check-equal? (moves-to-floor "()())" 2) 0)
--- day01/floor.rkt
+++ day01/floor.rkt
@@ -1,9 +1,29 @@
 #lang racket
-(define (where-is-santa floor-map)
-  (for/sum ([char floor-map])
-    (case char
-      [(#\() +1]
-      [(#\)) -1]
-      [else 0])))
+(provide where-is-santa
+         moves-until-floor)
 
-(provide where-is-santa)
+(define (where-is-santa floor-map)
+  (let-values ([(floor moves) (floor-walk floor-map +inf.0)])
+    floor))
+
+(define (moves-until-floor floor-map (goal-floor -1))
+  (let-values ([(floor moves) (floor-walk floor-map goal-floor)])
+    moves))
+
+(define (floor-walk floor-map goal-floor)
+  (let loop ([floor 0]
+             [moves 0]
+             [the-map (string->list floor-map)])
+    (cond [(= floor goal-floor) (values floor moves)]
+          [(empty? the-map) (values floor 0)]
+          [else
+           (loop (char->floor floor (first the-map))
+                 (add1 moves)
+                 (rest the-map))])))
+
+(define (char->floor floor char)
+  (+ floor
+     (case char
+       [(#\() +1]
+       [(#\)) -1]
+       [else 0])))
